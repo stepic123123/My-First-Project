75ed5e55055b1ca31c9c87303f35d7589f530da6
Vadym <7788853653x@gmail.com>
2024-06-04 18:39:19 +0300

    deleted text

--- cypress/e2e/Profile.cy.js
+++ cypress/e2e/Profile.cy.js
@@ -4,13 +4,13 @@ it ('login to profile ', { retries: 1 }, () => {
     
     cy.get('[data-cy="email-container"]')
             .find('input')
-            .invoke('val','7788853653x@gmail.com')
-            .should('have.value', '7788853653x@gmail.com');
+            .invoke('val','77444555333x@gmail.com')
+            .should('have.value', '77444555333x@gmail.com');
 
             cy.get('[data-cy="password-container"]')
             .find('input')
-            .invoke('val','qwerty12345678')
-            .should('have.value', 'qwerty12345678');
+            .invoke('val','wwweeettt')
+            .should('have.value', 'wwweeettt');
 
             // cy.get('[data-cy="login-submit"]').click({ force: true });
 
