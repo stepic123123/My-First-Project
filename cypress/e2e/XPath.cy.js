51898bfc5b4ec0b48c6815ca37f01093bbe3dcf6
Vadym <7788853653x@gmail.com>
2024-06-18 18:10:14 +0300

    added

--- cypress/e2e/XPath.cy.js
+++ cypress/e2e/XPath.cy.js
@@ -3,8 +3,6 @@ require('cypress-xpath');
 it('visit FAQ page and assert to FAQ balans', { retries: 1 }, () => {
     cy.visit('https://cosmolot.ua/faq');
 
-
-    //*[@id="app-body"]/div/div[1]/div/main/div/div/div/div/div[1]/button[2]
     cy.xpath(
         '//*[@id="app-body"]/div/div[1]/div/main/div/div/div/div/div[1]/button[2]'
     ).should('be.visible');
