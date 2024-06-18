fdb4b3ed8b0e87932bf22e0cb6fef0f8eb9887af
Vadym <7788853653x@gmail.com>
2024-06-18 15:17:59 +0300

    changed files

--- cypress/elements/mainList.js
+++ cypress/elements/mainList.js
@@ -1,58 +1,35 @@
 class MainList {
-    /* 
-        data-cy="burger-nav-list"
-    data-cy="main-menu-home-lnk”.  Головна
-    data-cy="main-menu-group-games-lnk" слоти
-    data-cy="main-menu-lobby-group-lnk" Live казино
-    data-cy="main-menu-group-instantgames-lnk" Швидкі ігри
-    data-cy="main-menu-offers-bonuses-lnk" Бонуси
-    data-cy="main-menu-tournaments-list-lnk" Турніри
-    data-cy="main-menu-promotions-lnk" Акції
-    data-cy="main-menu-livechat-btn" Чат 
-    data-cy="main-menu-faq-lnk" FAQ
-        */
     
-    
-    navList() {
+navList() {
         return cy.get('[data-cy="burger-nav-list"]');
     }
-
-    navMain() {
+navMain() {
         return cy.get('[data-cy="main-menu-home-lnk”]',{ timeout: 1000 });
     }
-
-    liveCasino() {
+liveCasino() {
         return cy.get('[data-cy="main-menu-lobby-group-lnk"]');
     }
-
-    slots() {
+slots() {
         return cy.get('[data-cy="main-menu-group-games-lnk"]');
     }
-
-    quickGames() {
+quickGames() {
         return cy.get('[data-cy="main-menu-group-instantgames-lnk"]');
     }
-
-    bonuses() {
+bonuses() {
         return cy.get('[data-cy="main-menu-offers-bonuses-lnk”]');
     }
-
-    tournaments() {
+tournaments() {
         return cy.get('[data-cy="main-menu-tournaments-list-lnk"]');
     }
-
-    promotions() {
+promotions() {
         return cy.get('[data-cy="main-menu-promotions-lnk"]');
     }
-
-    chat() {
+chat() {
         return cy.get('[data-cy="main-menu-livechat-btn"]');
     }
-
-    faq() {
+faq() {
         return cy.get('[data-cy="main-menu-faq-lnk"]');
     }
-
 }
 
 export default new MainList();
