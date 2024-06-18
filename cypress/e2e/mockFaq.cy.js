b522b0d316b6924635b34b95902c08368a88452f
Vadym <7788853653x@gmail.com>
2024-06-18 14:08:38 +0300

    changed file

--- cypress/e2e/mockFaq.cy.js
+++ cypress/e2e/mockFaq.cy.js
@@ -11,58 +11,40 @@ describe('This test will contain assertions for FAQ page', () => {
             });
         });
     });
+    
     it('visit FAQ page and assert to FAQ balans and all-questions ', { retries: 1 }, () => {
         cy.visit('https://cosmolot.ua/faq');
-        
-        cy.get('[data-cy="faq-balance"]').should('exist');
-        cy.get('[data-cy="faq-balance"]').should('be.visible');
-
-        
-        cy.get('[data-cy="faq-all-questions"]').should('exist');
-        cy.get('[data-cy="faq-all-questions"]').should('be.visible');
+    cy.get('[data-cy="faq-balance"]').should('exist');
+    cy.get('[data-cy="faq-balance"]').should('be.visible');
+    cy.get('[data-cy="faq-all-questions"]').should('exist');
+    cy.get('[data-cy="faq-all-questions"]').should('be.visible');
     });
 
     it('visit FAQ page and assert to FAQ verification and bonus ', { retries: 1 }, () => {
         cy.visit('https://cosmolot.ua/faq');
-        
-        cy.get('[data-cy="faq-verification"]').should('exist');
-        cy.get('[data-cy="faq-verification"]').should('be.visible');
-
-        
-        cy.get('[data-cy="faq-bonus-faq"]').should('exist');
-        cy.get('[data-cy="faq-bonus-faq"]').should('be.visible');
+    cy.get('[data-cy="faq-verification"]').should('exist');
+    cy.get('[data-cy="faq-verification"]').should('be.visible');
+    cy.get('[data-cy="faq-bonus-faq"]').should('exist');
+    cy.get('[data-cy="faq-bonus-faq"]').should('be.visible');
     });
 
     it('visit FAQ page and assert to FAQ payout and payin', { retries: 1 }, () => {
         cy.visit('https://cosmolot.ua/faq');
-        
-        
-        cy.get('[data-cy="faq-payout"]').should('exist');
-        cy.get('[data-cy="faq-payout"]').should('be.visible');
-    
-        
-        cy.get('[data-cy="faq-payin"]').should('exist');
-        cy.get('[data-cy="faq-payin"]').should('be.visible');
+    cy.get('[data-cy="faq-payout"]').should('exist');
+    cy.get('[data-cy="faq-payout"]').should('be.visible');
+    cy.get('[data-cy="faq-payin"]').should('exist');
+    cy.get('[data-cy="faq-payin"]').should('be.visible');
     });
 
     it('visit FAQ page and assert to FAQ profile, registration and contact', { retries: 1 }, () => {
         cy.visit('https://cosmolot.ua/faq');
-        
-        
-        cy.get('[data-cy="faq-profile"]').should('exist');
-        cy.get('[data-cy="faq-profile"]').should('be.visible');
-    
-       
-        cy.get('[data-cy="faq-registration"]').should('exist');
-        cy.get('[data-cy="faq-registration"]').should('be.visible');
-
-       
-        cy.get('[data-cy="faq-contact"]').should('exist');
-        cy.get('[data-cy="faq-contact"]').should('be.visible');
+    cy.get('[data-cy="faq-profile"]').should('exist');
+    cy.get('[data-cy="faq-profile"]').should('be.visible');
+    cy.get('[data-cy="faq-registration"]').should('exist');
+    cy.get('[data-cy="faq-registration"]').should('be.visible');
+    cy.get('[data-cy="faq-contact"]').should('exist');
+    cy.get('[data-cy="faq-contact"]').should('be.visible');
     });
-
-
-    
 });
 
 
