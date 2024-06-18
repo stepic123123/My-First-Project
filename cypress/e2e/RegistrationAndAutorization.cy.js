c6f345b0699e59939330dba79209b46ea2bf081f
Vadym <7788853653x@gmail.com>
2024-06-18 15:05:31 +0300

    changed files

--- cypress/e2e/RegistrationAndAutorization.cy.js
+++ cypress/e2e/RegistrationAndAutorization.cy.js
@@ -1,13 +1,9 @@
 it('Registration and autorization buttons ', { retries: 2 }, () => {
     cy.visit('https://cosmolot.ua/');
-
-    
-    cy.get('[data-cy="registration_lnk"]', { timeout: 1000 }).should('exist');
-    cy.get('[data-cy="registration_lnk"]', { timeout: 1000 }).should('be.visible');
-    cy.get('[data-cy="registration_lnk"]', { timeout: 1000 }).find('span').should('contain','Реєстрація');
-
-    cy.get('[data-cy="login_lnk"]', { timeout: 1000 }).should('exist');
-    cy.get('[data-cy="login_lnk"]', { timeout: 1000 }).should('be.visible');
-
-    
+cy.get('[data-cy="registration_lnk"]', { timeout: 1000 }).should('exist');
+cy.get('[data-cy="registration_lnk"]', { timeout: 1000 }).should('be.visible');
+cy.get('[data-cy="registration_lnk"]', { timeout: 1000 }).find('span').should('contain','Реєстрація');
+cy.get('[data-cy="login_lnk"]', { timeout: 1000 }).should('exist');
+cy.get('[data-cy="login_lnk"]', { timeout: 1000 }).should('be.visible');
 });
+
