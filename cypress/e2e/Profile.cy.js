b2c9a330bde7e35315b335509f4c82d2d97635eb
Vadym <7788853653x@gmail.com>
2024-06-18 14:57:28 +0300

    changed files

--- cypress/e2e/Profile.cy.js
+++ cypress/e2e/Profile.cy.js
@@ -2,19 +2,15 @@
 it ('login to profile ', { retries: 1 }, () => {
     cy.visit('https://cosmolot.ua/login');
     
-    cy.get('[data-cy="email-container"]')
+cy.get('[data-cy="email-container"]')
             .find('input')
             .invoke('val','77444555333x@gmail.com')
             .should('have.value', '77444555333x@gmail.com');
-
-            cy.get('[data-cy="password-container"]')
+cy.get('[data-cy="password-container"]')
             .find('input')
             .invoke('val','wwweeettt')
             .should('have.value', 'wwweeettt');
-
-            // cy.get('[data-cy="login-submit"]').click({ force: true });
-
-            cy.get('[data-cy="login-submit"]').find('span').click( 'multiple: true');
-
+cy.get('[data-cy="login-submit"]').find('span').click( 'multiple: true');
 });
+
 
